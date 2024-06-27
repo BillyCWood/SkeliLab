00fe9e6fca31c923d645706fa681d62b1386af4f
BillyCWood <billyc.wood4@gmail.com>
2024-06-26 19:56:38 -0600

    Added Modals that will appear when either of the Profile Header Buttons are pressed

--- components/ProfileHeaderButtons.js
+++ components/ProfileHeaderButtons.js
@@ -1,11 +1,83 @@
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { View, Text } from 'react-native';
+import { StyleSheet,View, Modal, Text, Dimensions, Pressable } from 'react-native';
+import { useState } from 'react';
+
+
 
 export default function ProfileHeaderButtons() {
+
+
+    const [modalVisibleSettings, setModalVisibleSettings] = useState(false);
+    const [modalVisibleMeasurements, setModalVisibleMeasurements] = useState(false);
+
+
     return(
         <View style={{flexDirection: 'row'}}>
-            <Ionicons name='settings-outline' onPress={() => alert('this is the settings button!')} style={{marginRight: 20, color: '#0A20FF'}} size={20} />
-            <Ionicons name='scale-outline' onPress={() => alert('this is the measurements button!')} style={{marginRight: 20, color: '#0A20FF'}} size={20} />
+            <Ionicons 
+                name='settings-outline'
+                onPress={
+                    () => setModalVisibleSettings(true)
+                }
+                style={{marginRight: 20, color: '#0A20FF'}} 
+                size={20} 
+            />
+            <Ionicons name='scale-outline' onPress={() => setModalVisibleMeasurements(true)} style={{marginRight: 20, color: '#0A20FF'}} size={20} />
+
+            <View>
+                <Modal
+                    visible={modalVisibleSettings}
+                    onRequestClose={() => setModalVisibleSettings(false)}
+                    animationType='slide'
+                    presentationStyle='pageSheet'
+                >
+                    <View style={styles.container}>
+                        <View style={styles.containerHeader}>
+                            <Pressable onPress={() => setModalVisibleSettings(false)} style={styles.closeButton} ><Ionicons name='close' size={20} color='white' /></Pressable>
+                            <Text style={{marginLeft: -45 + Dimensions.get('screen').width * .42}}>Settings</Text>
+                        </View>
+                    </View>
+              </Modal>
+            </View>
+
+
+            <View>
+                <Modal
+                    visible={modalVisibleMeasurements}
+                    onRequestClose={() => setModalVisibleMeasurements(false)}
+                    animationType='slide'
+                    presentationStyle='pageSheet'
+                >
+                    <View style={styles.container}>
+                        <View style={styles.containerHeader}>
+                            <Pressable onPress={() => setModalVisibleMeasurements(false)} style={styles.closeButton} ><Ionicons name='close' size={20} color='white' /></Pressable>
+                            <Text style={{marginLeft: -45 + Dimensions.get('screen').width * .39}}>Measurements</Text>
+                        </View>
+                    </View>
+              </Modal>
+            </View>
+
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 20,
+        marginLeft: 20,
+    },
+    containerHeader: {
+        flexDirection: 'row',
+    },
+    closeButton:{
+        backgroundColor:'lightgrey',
+        borderTopLeftRadius: 4,
+        borderTopRightRadius: 4,
+        borderBottomLeftRadius: 4,
+        borderBottomRightRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 25,
+        height: 20,
+    },
+})
