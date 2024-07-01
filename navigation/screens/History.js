5b162866a30f13fc43c85c3c03e36f8f9900ea53
Paxton Miller <paxtonmiller@Paxtons-MacBook-Pro.local>
2024-06-30 19:07:38 -0600

    modified:   navigation/screens/History.js

--- navigation/screens/History.js
+++ navigation/screens/History.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Calendar from "../../components/historyComponents/Calendar.js"
 
 export default function History({navigation}) {
   return (
